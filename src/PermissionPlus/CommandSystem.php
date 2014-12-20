501a85db94d369ba48a6d0e4e25ecf8c72ead704
Hmy2001 <harutowii@yahoo.co.jp>
2014-12-20 14:56:25 +0900

    CreateConfig,FormatConfig実装

--- src/PermissionPlus/CommandSystem.php
+++ src/PermissionPlus/CommandSystem.php
@@ -31,8 +31,61 @@ class CommandSystem{
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
         public function CreateConfig(){
-                //TODO
+                $this->Command = new Config($this->DataFolder."Command.yml", CONFIG::YAML, array("subcmd" => array( "ADMIN" => array(), "TRUST" => array(), "GUEST" => array(), ),"command" => array("ADMIN" => array( 'ban' => true, 'ban-ip' => true, 'banlist' => true, 'defaultgamemode' => true, 'deop' => true, 'difficulty' => true, 'gamemode' => true, 'give' => true, 'help' => true, 'kick' => true, 'kill' => true, 'list' => true, 'me' => true, 'op' => true, 'pardon' => true, 'pardon-ip' => true, 'plugins' => true, 'reload' => true, 'save-all' => true, 'save-off' => true, 'save-on' => true, 'say' =>true, 'seed' => true, 'setworldspawn' => true, 'spawnpoint' => true, 'status' => true, 'stop' => true, 'tell' => true, 'time' => true, 'timings' => true, 'tp' => true, 'version' => true, 'whitelist' => true, 'ppplayer' => true, 'ppcommand' => true, 'ppconfig' => true),"TRUST" => array( 'ban' => false, 'ban-ip' => false, 'banlist' => false, 'defaultgamemode' => false, 'deop' => true, 'difficulty' => false, 'gamemode' => false, 'give' => true, 'help' => true, 'kick' => true, 'kill' => false, 'list' => true, 'me' => true, 'op' => true, 'pardon' => false, 'pardon-ip' => false, 'plugins' => true, 'reload' => true, 'save-all' => false, 'save-off' => false, 'save-on' => false, 'say' => true, 'seed' => true, 'setworldspawn' => true, 'spawnpoint' => true, 'status' => true, 'stop' => false, 'tell' => true, 'time' => true, 'timings' => true, 'tp' => true, 'version' => false, 'whitelist' => false, 'ppplayer' => false, 'ppcommand' => false, 'ppconfig' => false),"GUEST" => array( 'ban' => false, 'ban-ip' => false, 'banlist' => false, 'defaultgamemode' => false, 'deop' => false, 'difficulty' => false, 'gamemode' => false, 'give' => false, 'help' => false, 'kick' => false, 'kill' => false, 'list' => true, 'me' => false, 'op' => false, 'pardon' => false, 'pardon-ip' => false, 'plugins' => false, 'reload' => false, 'save-all' => false, 'save-off' => false, 'save-on' => false, 'say' => false, 'seed' => false, 'setworldspawn' => false, 'spawnpoint' => false, 'status' => false, 'stop' => false, 'tell' => false, 'time' => false, 'timings' => false, 'tp' => false, 'version' => false, 'whitelist' => false, 'ppplayer' => false, 'ppcommand' => false, 'ppconfig' => false))));
+                $this->Command->save();
+                return true;
         }
+
+        public function FormatConfig(){
+                $config = new Config($this->DataFolder."config.yml", CONFIG::YAML);
+                if($config->get("version")){
+                        $version = $config->get("version");
+                        if(Main::VERSION > $version){
+                                $config = $config->getAll();
+                                foreach($config["subcmd"] as $per => $data){
+                                        if(!isset($this->Command->get("subcmd")[$per])){
+                                                $newcmd = array();
+                                                foreach ($this->Command->get("subcmd")["ADMIN"] as $cmd => $subcmds){
+                                                        $newcmd[$cmd] = array();
+                                                        foreach (array_keys($subcmds) as $sub){
+                                                                $newcmd[$cmd][$sub] = false;
+                                                        }
+                                                }
+                                                $this->Command->set("subcmd", array_merge($this->Command->get("subcmd"), array($per => $newcmd)));
+                                                unset($newcmd);
+                                        }
+                                        $newcmd = $this->Command->get('subcmd')[$per];
+                                        foreach($data as $cmd => $subcmds){
+                                                foreach($subcmds as $sub => $en){
+                                                        $newcmd[$cmd][$sub] = $en;
+                                                }
+                                        }
+                                        unset($this->Command->get('subcmd')[$per]);
+                                        $this->Command->set("subcmd", array_merge($this->Command->get("subcmd"), array($per => $newcmd)));
+                                }
+                                foreach($config["command"] as $per => $data){
+                                        if(!isset($this->Command->get("command")[$per])){
+                                                $this->Command->set("command", array_merge($this->Command->get("command"), array($per => array_fill_keys($this->getCommands(),false))));
+                                        }
+                                        $newcmd = $this->Command->get('command')[$per];
+                                        foreach($data as $cmd => $en){
+                                                $newcmd[$cmd]= $en;
+                                        }
+                                        unset($this->Command->get('command')[$per]);
+                                        $this->Command->set("command", array_merge($this->Command->get("command"), array($per => $newcmd)));
+                                }
+                                $this->Command->save();
+                                return true;
+                        }
+                        return false;
+                }
+                return false;
+        }
+
+
+
+
+
 
 //死ぬぽよ
 
--- src/PermissionPlus/PermissionSystem.php
+++ src/PermissionPlus/PermissionSystem.php
@@ -31,14 +31,39 @@ class PermissionSystem{
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
         public function CreateConfig(){
-                //TODO
+                $this->Account = new Config($this->DataFolder."Account.yml", CONFIG::YAML);
+                $this->Account->save();
+                $this->Permission = new Config($dataFolder."Permission.yml", CONFIG::YAML, array("notice" => true, "autoop" => false, "PerName" => false, "cmd-whitelist" => true, "permission" => array( "GUEST" => true, "TRUST" => true, "ADMIN" => true)));
+                $this->Permission->save();
+                return true;
         }
+
+        public function FormatConfig(){
+                $config = new Config($dataFolder."config.yml", CONFIG::YAML);
+                if($config->get("version")){
+                        $version = $config->get("version");
+                        if(Main::VERSION > $version){
+                                $config = $config->getAll();
+                                $this->Permission->set("notice", $config["notice"]);
+                                $this->Permission->set("autoop", $config["autoop"]);
+                                $this->Permission->set("cmd-whitelist", $config["cmd-whitelist"]);
+                                foreach($config["permission"] as $per => $en){
+                                        $this->Permission->set("permission", array_merge($this->Permission->get("permission"), array($per => $en)));
+                                }
+                                foreach($config["player"] as $player => $per){
+                                        $this->Account->set($player, array("Permission" => $per));
+                                }
+                                $this->Account->save();
+                                $this->Permission->save();
+                                return true;
+                        }
+                        return false;
+                }
+                return false;
+        }
+
+
 //死ぬぽよ
-
-
-
-
-
 
 
 
