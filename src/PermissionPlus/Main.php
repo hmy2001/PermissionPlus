cbc5360a9ae9e2bfd60d4c0750996dcdcceac925
Hmy2001 <harutowii@yahoo.co.jp>
2014-12-20 17:22:31 +0900

    コマンド全般追加完了

--- src/PermissionPlus/Main.php
+++ src/PermissionPlus/Main.php
@@ -44,13 +44,182 @@ class Main extends PluginBase implements Listener, CommandExecutor{
 		CommandSystem::API()->saveData();
 	}
 
-
 /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	コマンド
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
-//TODO
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		$username = $sender->getName();
+		switch($command->getName()){
+		case "pp":
+		$msg = PermissionSystem::API()->permissionUsage("p");
+		$sender->sendMessage("Usage: /ppplayer <player> $msg");
+		$msg = PermissionSystem::API()->permissionUsage("c");
+		$sender->sendMessage("Usage: /ppcommand <command or commandPermission> $msg");
+		$msg = PermissionSystem::API()->permissionUsage("c");
+		$sender->sendMessage("Usage: /ppsub <cmd> <subcmd> $msg");
+		$sender->sendMessage("Usage: /ppconfig");
+		break;
+		case "ppplayer":
+		$player = array_shift($args);
+		$permission = array_shift($args);
+		if(is_null($player) or is_null($permission)){
+			if($username === "CONSOLE"){
+                		PermissionSystem::API()->showPPermissionsList($sender);
+			}
+                	$msg = PermissionSystem::API()->permissionUsage("p");
+                	$sender->sendMessage("Usage: /ppplayer <player> $msg");
+		break;
+		}
+                PermissionSystem::API()->setPPermission($player, $permission,$sender);
+                PermissionSystem::API()->saveData();
+                $player = $sender->getServer()->getPlayerExact($player);
+                if($player instanceof Player){
+			$this->setPermission($player);
+			$this->changeName($player);
+		}
+                break;
+		case "ppcommand":
+                $command = array_shift($args);
+		if(is_null($command)) {
+                	if($username === "CONSOLE"){
+                		CommandSystem::API()->showCPermissionsList($sender);
+                	}
+                	$msg = PermissionSystem::API()->permissionUsage("c");
+                	$sender->sendMessage("Usage: /ppcommand <command or commandPermission> $msg");
+                break;
+                }
+                CommandSystem::API()->setCPermission($command,$args,$sender);
+                CommandSystem::API()->saveData();
+                foreach(Server::getInstance()->getOnlinePlayers() as $player){
+                	$this->setPermission($player);
+                }
+                break;
+		case "ppsub":
+                $cmd = array_shift($args);
+                $subcmd = array_shift($args);
+                if(is_null($cmd) or is_null($subcmd)){
+                	if ($username === "CONSOLE") {
+                		CommandSystem::API()->showSPermissionsList($sender);
+                	}
+                        $msg = PermissionSystem::API()->permissionUsage("c");
+                        $sender->sendMessage("Usage: /ppsub <cmd> <subcmd> $msg");
+                break;
+                }
+                CommandSystem::API()->setSPermission($cmd, $subcmd, $args, $sender);
+                CommandSystem::API()->saveData();
+                foreach(Server::getInstance()->getOnlinePlayers() as $player){
+                	$this->setPermission($player);
+                }
+                break;
+                case "ppconfig":
+		$config = array_shift($args);
+           	switch($config){
+           	case "notice":
+		$bool = array_shift($args);
+		if(!$bool = $this->castBool($bool)){
+			$sender->sendMessage("Usage: /ppconfig notice <on | off>");
+		break;
+		}
+                PermissionSystem::API()->set("notice", $bool);
+                PermissionSystem::API()->saveData();
+                if($bool){
+                	$sender->sendMessage("[Permission+] Truned on to the notify function.");
+                }else{
+                	$sender->sendMessage("[Permission+] Truned off to the notify function.");
+                }
+                break;
+                case "autoop":
+                $bool = array_shift($args);
+                if(!$bool = $this->castBool($bool)){
+                	$sender->sendMessage("Usage: /ppconfig autoop <on | off>");
+                break;
+                }
+                PermissionSystem::API()->set("autoop", $bool);
+                PermissionSystem::API()->saveData();
+                if($bool){
+                	$sender->sendMessage("[Permission+] Truned on to the auto op function.");
+                        $this->giveOPtoEveryone();
+                }else{
+                	$sender->sendMessage("[Permission+] Truned off to the auto op function.");
+                }
+                break;
+		case "pername":
+		$bool = array_shift($args);
+		if(!$bool = $this->castBool($bool)){
+                	$sender->sendMessage("Usage: /ppconfig pername <on | off>");
+                break;
+                }
+                PermissionSystem::API()->set("PerName", $bool);
+                PermissionSystem::API()->saveData();
+                if($bool){
+                	$sender->sendMessage("[Permission+] Truned on to the PerName function.");
+                	$this->changeNametoEveryone();
+                }else{
+                	$sender->sendMessage("[Permission+] Truned off to the PerName function.");
+                }
+                break;
+                case "cmdwhitelist":
+                case "cmdw":
+                $bool = array_shift($args);
+                if(!$bool = $this->castBool($bool)){
+                	$sender->sendMessage("Usage: /ppconfig cmdwhitelist <on | off>");
+                break;
+                }
+                PermissionSystem::API()->set("cmdw", $bool);
+                PermissionSystem::API()->saveData();
+                if($bool){
+                	$sender->sendMessage("[Permission+] Truned on to the cmd-whitelist function.");
+                }else{
+                	$sender->sendMessage("[Permission+] Truned off to the cmd-whitelist function.");
+                	$sender->sendMessage("".TextFormat::DARK_RED."[Permission+] You have to restart PocketMine-MP to apply the setting!".TextFormat::WHITE."");
+                }
+                break;
+                case "add":
+                $name = array_shift($args);
+                if(empty($name) || !$this->isAlnum($name)){
+                	$sender->sendMessage("Usage: /ppconfig add <rank name>");
+                break;
+                }
+                if(PermissionSystem::API()->addPermission($name)){
+                	$sender->sendMessage("[Permission+] Successful!");
+                }else{
+                	$sender->sendMessage("[Permission+] Failed to add!");
+                }
+                break;
+                case "rm":
+                case "remove":
+                $name = array_shift($args);
+                if(empty($name) || !$this->isAlnum($name)){
+                	$sender->sendMessage("Usage: /ppconfig remove <rank name>");
+                break;
+                }
+                if(PermissionSystem::API()->removePermission($name)){
+                	$sender->sendMessage("[Permission+] Successful!");
+                }else{
+                	$sender->sendMessage("[Permission+] Failed to remove!");
+                }
+                break;
+                case "":
+		default:
+		$sender->sendMessage("Usage: /ppconfig notice <on | off>");
+		$sender->sendMessage("Usage: /ppconfig autoop <on | off>");
+		$sender->sendMessage("Usage: /ppconfig pername <on | off>");
+		$sender->sendMessage("Usage: /ppconfig add <rank name>");
+		$sender->sendMessage("Usage: /ppconfig remove <rank name>");
+		break;
+		}
+                return true;
+                break;
+		}
+        }
 
+
+
+
+
+
+//TODO
 
 
 
